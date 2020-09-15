ec22a54004f2bcb51ce42a7ba244785f8a589dbe
Fredrik Engstrand <frewacom@gmail.com>
2020-09-15 14:31:23 +0200

    ass1: add tests for ioopm_hash_table_insert

--- inluppar/inlupp1/hash_table_tests.c
+++ inluppar/inlupp1/hash_table_tests.c
@@ -34,9 +34,10 @@ void test_lookup() {
 }
 
 // Test if an invalid key gives an error
-void test_lookup1() {
+void test_lookup_gives_error() {
   ioopm_hash_table_t *ht = ioopm_hash_table_create();
 
+  // Lookup a key that does not exist (since the hash table is empty)
   char *value = ioopm_hash_table_lookup(ht, 1);
   CU_ASSERT(errno == EINVAL);
   CU_ASSERT_PTR_NULL(value);
@@ -44,8 +45,59 @@ void test_lookup1() {
   ioopm_hash_table_destroy(ht);
 }
 
-// TODO: Add test for checking that a key exist after being inserted
-// TODO: Add test for checking that a key does not exist after being removed
+void test_insert() {
+  ioopm_hash_table_t *ht = ioopm_hash_table_create();
+
+  int dummy_key = 1;
+  char *dummy_value = "test";
+
+  // Insert a dummy entry with key 1
+  ioopm_hash_table_insert(ht, dummy_key, dummy_value);
+  char *value = ioopm_hash_table_lookup(ht, dummy_key);
+
+  // 'errno' gets reset to 0 when calling 'ioopm_hash_table_lookup', so assuming
+  // that there were no errors, errno will be 0.
+  CU_ASSERT_EQUAL(errno, 0);
+  CU_ASSERT_EQUAL(value, dummy_value);
+
+  ioopm_hash_table_destroy(ht);
+}
+
+void test_insert_replace() {
+  ioopm_hash_table_t *ht = ioopm_hash_table_create();
+
+  int dummy_key = 1;
+  char *initial_value = "test";
+  char *replaced_value = "hello";
+
+  ioopm_hash_table_insert(ht, dummy_key, initial_value);
+
+  // Replace the value of the entry with 'dummy_key' as key
+  ioopm_hash_table_insert(ht, dummy_key, replaced_value);
+
+  char *value = ioopm_hash_table_lookup(ht, dummy_key);
+
+  // Make sure that the value was changed to 'replaced_value'
+  // and that there were no errors
+  CU_ASSERT_EQUAL(errno, 0);
+  CU_ASSERT_EQUAL(value, replaced_value);
+
+  ioopm_hash_table_destroy(ht);
+}
+
+// TODO: Not sure if this can be tested since lookup does not accept keys < 0
+//       The same method should be used when inserting.
+/* void test_insert_invalid_key() { */
+/*   ioopm_hash_table_t *ht = ioopm_hash_table_create(); */
+
+/*   int invalid_key = -1; */
+/*   ioopm_hash_table_insert(ht, invalid_key, NULL); */
+/*   char *value = ioopm_hash_table_lookup(ht, invalid_key); */
+
+/*   CU_ASSERT_EQUAL(errno, EINVAL); */
+
+/*   ioopm_hash_table_destroy(ht); */
+/* } */
 
 int main() {
   CU_pSuite test_suite1 = NULL;
@@ -62,7 +114,9 @@ int main() {
   if (
     (NULL == CU_add_test(test_suite1, "it creates and returns a pointer to an allocated hash table", test_create_destroy)) ||
     (NULL == CU_add_test(test_suite1, "it returns NULL for keys that does not exist", test_lookup)) ||
-    (NULL == CU_add_test(test_suite1, "it returns an error for keys that does not exist", test_lookup1))
+    (NULL == CU_add_test(test_suite1, "it returns an error for keys that does not exist", test_lookup_gives_error)) ||
+    (NULL == CU_add_test(test_suite1, "it inserts a new entry at specified key", test_insert)) ||
+    (NULL == CU_add_test(test_suite1, "it replaces an entry when inserting with existing key", test_insert_replace))
    ) {
     CU_cleanup_registry();
     return CU_get_error();
